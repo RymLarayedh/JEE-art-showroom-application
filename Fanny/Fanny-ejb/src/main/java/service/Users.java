ce8819cc8beb6f31d0e3473a742ac709a21bf025
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2017-02-22 21:11:01 +0100

    Aymen pushed this

---
+++ Fanny/Fanny-ejb/src/main/java/service/Users.java
@@ -0,0 +1,18 @@
+package service;
+
+import javax.ejb.Stateful;
+
+/**
+ * Session Bean implementation class Users
+ */
+@Stateful
+public class Users implements UsersRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public Users() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
