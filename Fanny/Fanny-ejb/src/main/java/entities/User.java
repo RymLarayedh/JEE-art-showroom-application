5fe9e52ec7adc4e4dff427c9b76b175316761480
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2017-02-23 18:34:09 +0100

    23/02/2017-18:34

---
+++ Fanny/Fanny-ejb/src/main/java/entities/User.java
@@ -0,0 +1,71 @@
+package entities;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	   
+	@Id
+	private int idUser;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String email;
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+		super();
+	}   
+	public int getIdUser() {
+		return this.idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}   
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}   
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}   
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}   
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}   
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+   
+}
