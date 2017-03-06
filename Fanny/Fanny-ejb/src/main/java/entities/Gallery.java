72a6fd8400cf0f980a075249be65e5aca85a15af
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2017-03-06 21:08:51 +0100

    Signed-off-by: ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>

--- Fanny/Fanny-ejb/src/main/java/entities/Gallery.java
+++ Fanny/Fanny-ejb/src/main/java/entities/Gallery.java
@@ -4,6 +4,7 @@ import entities.User;
 import java.io.Serializable;
 import java.lang.String;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.*;
 
@@ -17,7 +18,7 @@ public class Gallery extends User implements Serializable {
 
 	private float Surface;
 	private String Description;
-	// private List<>
+	//private Set<byte[]> PhotoAlbum;
 	private static final long serialVersionUID = 1L;
 	@OneToMany
 	private List<Event>listEvent;
@@ -45,5 +46,13 @@ public class Gallery extends User implements Serializable {
 	public void setDescription(String Description) {
 		this.Description = Description;
 	}
+
+/*	public Set<byte[]> getPhotoAlbum() {
+		return PhotoAlbum;
+	}
+
+	public void setPhotoAlbum(Set<byte[]> photoAlbum) {
+		PhotoAlbum = photoAlbum;
+	}*/
 
 }
