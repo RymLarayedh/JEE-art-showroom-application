9c4ec1785cb7266ea4dbcc0f25db3c74cb5426b2
Dell <Dell@192.168.30.1>
2017-03-07 17:07:52 +0100

    Signed-off-by: manel <Dell@192.168.30.1>

---
+++ Fanny/Fanny-ejb/src/main/java/entities/VisualArt.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+
+
+public class VisualArt extends Artwork implements Serializable {
+	
+	
+	private int length;
+	private int width;
+	
+	private static final long serialVersionUID = 1L;
+	@OneToOne()
+	@JoinColumn(name="category_art")
+	private Category category;
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+
+
+}
