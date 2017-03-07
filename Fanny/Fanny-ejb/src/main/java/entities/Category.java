bc1bb4f86b7d476d8f4948a4f75c3e27e534f187
Dell <Dell@DESKTOP-QEROJRR>
2017-03-07 14:53:23 +0100

    Signed-off-by: manel <Dell@DESKTOP-QEROJRR>

--- Fanny/Fanny-ejb/src/main/java/entities/Category.java
+++ Fanny/Fanny-ejb/src/main/java/entities/Category.java
@@ -31,8 +31,8 @@ public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	@OneToOne(mappedBy="category")
-	private Artwork visualArt ;
+//	@OneToOne(mappedBy="category")
+//	private Artwork visualArt ;
 	
 	public Category() {
 		super();
