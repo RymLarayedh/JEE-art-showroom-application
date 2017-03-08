380ce805d542d6807f9821fe63b6240e554bf80c
RymLarayedh <rymlarayedh@172.19.12.105>
2017-03-09 00:15:07 +0100

    YES
    Signed-off-by: RymLarayedh <rymlarayedh@172.19.12.105>

--- ClientFanny/src/main/java/applicationRym/Main.java
+++ ClientFanny/src/main/java/applicationRym/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             primaryStage.setOnCloseRequest(e ->{
                 e.consume();
                 closeProgram(primaryStage);
+                //r
             });
             primaryStage.show();
 			
