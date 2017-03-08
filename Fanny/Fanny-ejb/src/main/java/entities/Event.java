349fdbfe531899ecc431f452120128e7963ccbc7
RymLarayedh <rymlarayedh@172.19.12.105>
2017-03-08 21:19:33 +0100

    idGenerated Event
    Signed-off-by: RymLarayedh <rymlarayedh@172.19.12.105>

--- Fanny/Fanny-ejb/src/main/java/entities/Event.java
+++ Fanny/Fanny-ejb/src/main/java/entities/Event.java
@@ -16,6 +16,7 @@ public class Event implements Serializable {
 //rym//Aymen
 	   
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idEvent;
 	private String title;
 	private String description;
