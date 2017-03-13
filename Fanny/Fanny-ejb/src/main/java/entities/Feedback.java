5c2b013ced67d29b3f7b20712dcc059ed146c73d
RymLarayedh <rym.larayedh@esprit.tn>
2017-03-13 12:35:52 +0100

    rabbi yehdi elli badal'ha hal classe hedhi
    Signed-off-by: RymLarayedh <rym.larayedh@esprit.tn>

--- Fanny/Fanny-ejb/src/main/java/entities/Feedback.java
+++ Fanny/Fanny-ejb/src/main/java/entities/Feedback.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -19,6 +21,7 @@ public class Feedback implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int  feedbackId ;
 	@ManyToOne
 	private User user ;
